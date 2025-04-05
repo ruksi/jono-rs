bec5b838b09f36e1933254c20de8418aeb3a5f17
Ruksi Korpisara <me@ruk.si>
2025-04-05 23:14:29 +0300

    🧪 Add more tests for the job maps
    will probably prune tests a bit after I have more confidence
    how everything will look

--- jono/tests/test_inspect.rs
+++ jono/tests/test_inspect.rs
@@ -12,14 +12,18 @@ async fn test_job_not_found_for_metadata() {
     let inspector = Inspector::with_context(context);
     let unknown_job_id = generate_job_id();
     assert!(matches!(
-        inspector.get_job_metadata(&unknown_job_id).await.err().unwrap(),
+        inspector
+            .get_job_metadata(&unknown_job_id)
+            .await
+            .err()
+            .unwrap(),
         JonoError::JobNotFound(_)
     ));
 }
 
 #[tokio::test]
-async fn test_get_current_jobs() -> Result<()> {
-    let context = create_test_context("test_sorted_sets");
+async fn test_job_maps_on_all() -> Result<()> {
+    let context = create_test_context("test_maps_on_all");
     let inspector = Inspector::with_context(context.clone());
 
     let now = current_timestamp_ms();
@@ -29,8 +33,12 @@ async fn test_get_current_jobs() -> Result<()> {
     let cancel_fix = JobFixture::new(context.clone(), JobStatus::Canceled, now + 30000).await?;
     let harvest_fix = JobFixture::new(context.clone(), JobStatus::Harvestable, now).await?;
 
-    let job_ids = inspector.get_status_to_job_ids(JobFilter::default()).await?;
-    let job_metadatas = inspector.get_status_to_job_metadata(JobFilter::default()).await?;
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
 
     assert_eq!(job_ids.queued, vec![queue_fix.job_id.clone()]);
     assert_eq!(job_ids.running, vec![run_fix.job_id.clone()]);
@@ -42,6 +50,155 @@ async fn test_get_current_jobs() -> Result<()> {
     assert_eq!(job_metadatas.running.len(), 1);
     assert_eq!(job_metadatas.scheduled.len(), 1);
     assert_eq!(job_metadatas.canceled.len(), 1);
+    assert_eq!(job_metadatas.harvestable.len(), 1);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_job_maps_on_queued() -> Result<()> {
+    let context = create_test_context("test_maps_on_queued");
+    let inspector = Inspector::with_context(context.clone());
+
+    let queue_fix = JobFixture::new(context.clone(), JobStatus::Queued, 0).await?;
+
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
+
+    assert_eq!(job_ids.queued, vec![queue_fix.job_id.clone()]);
+    assert!(job_ids.running.is_empty());
+    assert!(job_ids.scheduled.is_empty());
+    assert!(job_ids.canceled.is_empty());
+    assert!(job_ids.harvestable.is_empty());
+
+    assert_eq!(job_metadatas.queued.len(), 1);
+    assert_eq!(job_metadatas.running.len(), 0);
+    assert_eq!(job_metadatas.scheduled.len(), 0);
+    assert_eq!(job_metadatas.canceled.len(), 0);
+    assert_eq!(job_metadatas.harvestable.len(), 0);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_job_maps_on_running() -> Result<()> {
+    let context = create_test_context("test_maps_on_running");
+    let inspector = Inspector::with_context(context.clone());
+
+    let now = current_timestamp_ms();
+    let run_fix = JobFixture::new(context.clone(), JobStatus::Running, now + 10000).await?;
+
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
+
+    assert!(job_ids.queued.is_empty());
+    assert_eq!(job_ids.running, vec![run_fix.job_id.clone()]);
+    assert!(job_ids.scheduled.is_empty());
+    assert!(job_ids.canceled.is_empty());
+    assert!(job_ids.harvestable.is_empty());
+
+    assert_eq!(job_metadatas.queued.len(), 0);
+    assert_eq!(job_metadatas.running.len(), 1);
+    assert_eq!(job_metadatas.scheduled.len(), 0);
+    assert_eq!(job_metadatas.canceled.len(), 0);
+    assert_eq!(job_metadatas.harvestable.len(), 0);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_job_maps_on_scheduled() -> Result<()> {
+    let context = create_test_context("test_maps_on_scheduled");
+    let inspector = Inspector::with_context(context.clone());
+
+    let now = current_timestamp_ms();
+    let schedule_fix = JobFixture::new(context.clone(), JobStatus::Scheduled, now + 60000).await?;
+
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
+
+    assert!(job_ids.queued.is_empty());
+    assert!(job_ids.running.is_empty());
+    assert_eq!(job_ids.scheduled, vec![schedule_fix.job_id.clone()]);
+    assert!(job_ids.canceled.is_empty());
+    assert!(job_ids.harvestable.is_empty());
+
+    assert_eq!(job_metadatas.queued.len(), 0);
+    assert_eq!(job_metadatas.running.len(), 0);
+    assert_eq!(job_metadatas.scheduled.len(), 1);
+    assert_eq!(job_metadatas.canceled.len(), 0);
+    assert_eq!(job_metadatas.harvestable.len(), 0);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_job_maps_on_canceled() -> Result<()> {
+    let context = create_test_context("test_maps_on_canceled");
+    let inspector = Inspector::with_context(context.clone());
+
+    let now = current_timestamp_ms();
+    let cancel_fix = JobFixture::new(context.clone(), JobStatus::Canceled, now + 30000).await?;
+
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
+
+    assert!(job_ids.queued.is_empty());
+    assert!(job_ids.running.is_empty());
+    assert!(job_ids.scheduled.is_empty());
+    assert_eq!(job_ids.canceled, vec![cancel_fix.job_id.clone()]);
+    assert!(job_ids.harvestable.is_empty());
+
+    assert_eq!(job_metadatas.queued.len(), 0);
+    assert_eq!(job_metadatas.running.len(), 0);
+    assert_eq!(job_metadatas.scheduled.len(), 0);
+    assert_eq!(job_metadatas.canceled.len(), 1);
+    assert_eq!(job_metadatas.harvestable.len(), 0);
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_job_maps_on_harvestable() -> Result<()> {
+    let context = create_test_context("test_maps_on_harvestable");
+    let inspector = Inspector::with_context(context.clone());
+
+    let now = current_timestamp_ms();
+    let harvest_fix = JobFixture::new(context.clone(), JobStatus::Harvestable, now).await?;
+
+    let job_ids = inspector
+        .get_status_to_job_ids(JobFilter::default())
+        .await?;
+    let job_metadatas = inspector
+        .get_status_to_job_metadata(JobFilter::default())
+        .await?;
+
+    assert!(job_ids.queued.is_empty());
+    assert!(job_ids.running.is_empty());
+    assert!(job_ids.scheduled.is_empty());
+    assert!(job_ids.canceled.is_empty());
+    assert_eq!(job_ids.harvestable, vec![harvest_fix.job_id.clone()]);
+
+    assert_eq!(job_metadatas.queued.len(), 0);
+    assert_eq!(job_metadatas.running.len(), 0);
+    assert_eq!(job_metadatas.scheduled.len(), 0);
+    assert_eq!(job_metadatas.canceled.len(), 0);
     assert_eq!(job_metadatas.harvestable.len(), 1);
 
     Ok(())
